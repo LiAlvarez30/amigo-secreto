77e7ca60ff1e3e0264bdec9b277864c575bc074e
Liliana Alvarez <lialvarez0630@gmail.com>
2025-08-13 13:00:16 -0300

    Primer commit - Juego Amigo Secreto

---
+++ README.md
@@ -0,0 +1,33 @@
+# 🎁 Challenge Amigo Secreto - Alura Oracle
+
+En este desafío, desarrollé una aplicación que permitira a los usuarios ingresar nombres de amigos en una lista para luego realizar un sorteo aleatorio y determinar quién es el "amigo secreto".
+
+El usuario deberá agregar nombres mediante un campo de texto y un botón "Añadir". Los nombres ingresados se mostrarán en una lista visible en la página, y al finalizar, un botón "Sortear Amigo" seleccionará uno de los nombres de forma aleatoria, mostrando el resultado en pantalla.
+
+## 🚀 Funcionalidades
+- Agregar nombres a una lista.
+- Validar que no se agreguen campos vacíos.
+- Mostrar la lista en pantalla.
+- Sortear un amigo de forma aleatoria.
+
+## 📂 Estructura
+- **index.html** → Estructura de la página.
+- **script.js** → Lógica de programación.
+- **style.css** (opcional) → Estilos.
+
+## 🛠 Tecnologías utilizadas
+- HTML
+- CSS
+- JavaScript
+
+## 📌 Cómo usar
+1. Escribe un nombre en el campo de texto.
+2. Haz clic en "Adicionar" para agregarlo a la lista.
+3. Cuando tengas todos los nombres, haz clic en "Sortear Amigo".
+4. El sistema mostrará un nombre aleatorio como el "Amigo Secreto".
+
+## 🌐 Demo en GitHub Pages
+[Enlace al proyecto](https:)
+
+---
+Hecho para el challenge de **#AluraLatam** y **#OracleNextEducation**
---
+++ app.js
@@ -0,0 +1,50 @@
+
+// Lista donde se van a guardar los nombres
+let listaAmigos = [];
+
+
+// Función para agregar un amigo a la lista
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor escribe un nombre.");
+        return;
+    }
+
+    listaAmigos.push(nombre);
+    input.value = "";
+    mostrarLista();
+}
+
+// Funcion para mostrar los nombres en la lista 
+function mostrarLista() {
+    let ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+
+    for (let i = 0; i < listaAmigos.length; i++) {
+        let li = document.createElement("li");
+        // En esta linea de codigo muestro con indice el nombre.
+        li.textContent = `${i + 1}. ${listaAmigos[i]}`;
+        ul.appendChild(li);
+    }
+}
+
+// Función para sortear un amigo al azar
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        alert("No hay Amigos para sortear. Agrega al menos un amigo.");
+        return;
+    }
+
+    let indice = Math.floor(Math.random() * listaAmigos.length);
+    let elegido = listaAmigos[indice];
+
+    let resultado = document.getElementById("resultado");
+
+    // Actualizo resultado (este es un <ul> en el HTML), muestro un <li> con el resultado
+    resultado.innerHTML = `<li> El amigo secreto es: <strong>${elegido}</strong></li>`;
+}
+
+
